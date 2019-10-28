fdbfdc91f081cd487b039ba023cd3d19cef6c150
Allen Shin <geeuho@gmail.com>
2019-10-28 05:52:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves) 
+  dwarves.each_with_index do |dwarf, index|
+    puts "/#{index + 1}.*#{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(summons)
+  summons.map do |summon| 
+    "#{summon}!".capitalize()
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words)
+  if words.length <= 4
+    return true
+  else
+    return false
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheeses)
   cheese_types = ["cheddar", "gouda", "camembert"]
+    (cheeses).find { |i| i == cheese_types }
 end
